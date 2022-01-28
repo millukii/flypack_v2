fda7f2cd5e44fc32489a23963908b1192bf823d9
mquezadab2b <81397300+mquezadab2b@users.noreply.github.com>
2022-01-27 22:16:05 -0300

    Update CPrices.php

--- application/controllers/CPrices.php
+++ application/controllers/CPrices.php
@@ -212,20 +212,21 @@ class CPrices extends CI_Controller {
 		$objPHPExcel->setActiveSheetIndex(0);
 
 		$objPHPExcel->getActiveSheet()->SetCellValue('A1', "");
-
+		$indice = 0;
 		for($i=0; $i < count($prices); $i++)
 		{
 			if(!in_array($prices[$i]['from'], $from))
 			{
 				array_push($from, $prices[$i]['from']);
-				$objPHPExcel->getActiveSheet()->SetCellValue('A'.($i+1),$prices[$i]['from']);
-				$objPHPExcel->getActiveSheet()->SetCellValue($this->letters[($i+1)].'1',$prices[$i]['from']);
-				$objPHPExcel->getActiveSheet()->SetCellValue($this->letters[($i+1)].($i+1),$prices[$i]['value']);
+				$objPHPExcel->getActiveSheet()->SetCellValue('A'.($indice+1),$prices[$indice]['from']);
+				$objPHPExcel->getActiveSheet()->SetCellValue($this->letters[($indice+1)].'1',$prices[$indice]['from']);
+				$objPHPExcel->getActiveSheet()->SetCellValue($this->letters[($indice+1)].($indice+1),$prices[$indice]['value']);
 			}
 			else
 			{
-				$objPHPExcel->getActiveSheet()->SetCellValue($this->letters[($i+1)].'1',$prices[$i]['from']);
-				$objPHPExcel->getActiveSheet()->SetCellValue($this->letters[($i+1)].($i+1),$prices[$i]['value']);
+				$indice++;
+				$objPHPExcel->getActiveSheet()->SetCellValue($this->letters[($indice+1)].'1',$prices[$indice]['from']);
+				$objPHPExcel->getActiveSheet()->SetCellValue($this->letters[($indice+1)].($indice+1),$prices[$indice]['value']);
 			}
 		}
 
