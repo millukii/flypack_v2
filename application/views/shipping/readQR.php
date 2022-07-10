fbb7bc27384dbd6eb9b2f4baf31d9bee734938fa
mquezadab2b <81397300+mquezadab2b@users.noreply.github.com>
2022-07-09 22:51:33 -0400

    Update readQR.php

--- application/views/shipping/readQR.php
+++ application/views/shipping/readQR.php
@@ -120,6 +120,8 @@
                 }
             });
         }
+
+        alert(`Code scanned = ${decodedText}`, decodedResult);
     }
     var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 10, qrbox: 250 });
     html5QrcodeScanner.render(onScanSuccess);
