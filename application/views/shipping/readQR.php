605aac357d6fde5836292ba7da55ef9cfbac0170
mquezadab2b <81397300+mquezadab2b@users.noreply.github.com>
2022-07-09 22:27:21 -0400

    Update readQR.php

--- application/views/shipping/readQR.php
+++ application/views/shipping/readQR.php
@@ -109,6 +109,8 @@
 
     function onScanSuccess(decodedText, decodedResult) {
         console.log(`Code scanned = ${decodedText}`, decodedResult);
+
+        alert(`Code scanned = ${decodedText}`, decodedResult);
     }
     var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 10, qrbox: 250 });
     html5QrcodeScanner.render(onScanSuccess);
