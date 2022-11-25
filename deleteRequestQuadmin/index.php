70fc02195a418f2ac034e26c7a962283e66785b9
mquezadab2b <81397300+mquezadab2b@users.noreply.github.com>
2022-11-25 13:25:15 -0300

    Update index.php

--- deleteRequestQuadmin/index.php
+++ deleteRequestQuadmin/index.php
@@ -1,5 +1,16 @@
 <?php
-include_once '/home/flypackroot/public_html/flypack_v2/deleteRequestQuadmin/conexion.php';
+$host   = 'localhost';
+$dbname = 'flypackroot_flypack';
+$dbuser = 'flypackroot_flypack';
+$dbpass = 'Leica666_';
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+try {
+    $conexion = new PDO('mysql:host='.$host.';dbname='.$dbname, $dbuser, $dbpass);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "ERROR: " . $e->getMessage();
+}
 ini_set('date.timezone', 'America/Santiago');
 //delete
 $SQL = "delete from shipping_request where status = 1";
