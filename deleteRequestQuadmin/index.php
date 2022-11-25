395f48773a6a78a4089e5ec87c42fd618897f3d6
mquezadab2b <81397300+mquezadab2b@users.noreply.github.com>
2022-11-25 13:24:29 -0300

    varios

--- deleteRequestQuadmin/index.php
+++ deleteRequestQuadmin/index.php
@@ -1,5 +1,5 @@
 <?php
-include_once '/home/flypackroot/public_html/flypack_v2/updatePois/conexion.php';
+include_once '/home/flypackroot/public_html/flypack_v2/deleteRequestQuadmin/conexion.php';
 ini_set('date.timezone', 'America/Santiago');
 //delete
 $SQL = "delete from shipping_request where status = 1";
